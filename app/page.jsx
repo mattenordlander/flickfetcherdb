f603feb6562fcce89ccdbbf66567373647a163b7
Mathias Nordlander <mattsson.nord@gmail.com>
2024-03-18 21:21:55 +0100

    Refactor code from page to MovieSearchForm

--- app/page.jsx
+++ app/page.jsx
@@ -4,7 +4,7 @@ import { useEffect, useState } from "react";
 import { getMovieList, getSearchedMovie } from "./api/api";
 import Link from "next/link";
 import { useDebounce } from "use-debounce";
-import SelectElement from "./components/SelectElement"
+import MovieSearchForm from "./components/MovieSearchForm"
 
 export default function Home() {
   const [movieList, setMovieList] = useState([]);
@@ -60,57 +60,42 @@ export default function Home() {
     return <h1>Loading...</h1>;
   }
   return (
-    <main>
-      <label htmlFor="Search">Title...</label>
-      <input
-        type="text"
-        onChange={handleChange}
-        value={searchMovieValue.movieTitle}
-        name={"movieTitle"}
-      />
-     <SelectElement
-     label={"Year"}
-     handleChange={handleChange}
-     value={searchMovieValue.releaseDate}
-     yearsArray={yearsArray}
-     disabled={searchMovieValue.movieTitle.length <= 1}
-     />
-      <button
-        style={{
-          border: "1px solid black",
-          padding: ".1rem .7rem",
-          marginLeft: "1rem",
-        }}
-        onClick={() => {
-          setLoading(true)
-          fetchSearchedMovies(
-            searchMovieValue.movieTitle,
-            searchMovieValue.releaseDate
-          );
-        }}
-        disabled={searchMovieValue.movieTitle.length <= 1}
-      >
-        Search
-      </button>
-      <h1>TRENDNING MOVIES</h1>
-      {movieList.length === 0 && <><h1>Your search - did not match any movies</h1></>}
+    <main  className="m-auto container px-4">
+        
+             <MovieSearchForm
+             
+             yearLabel={"Year"}
+             inputLabel={"What are you looking for?"}
+             handleChange={handleChange}
+             value={searchMovieValue.releaseDate}
+             titleValue={searchMovieValue.movieTitle}
+             yearsArray={yearsArray}
+             disabled={searchMovieValue.movieTitle.length <= 1}
+             disabledBtn={searchMovieValue.movieTitle.length <= 1}
+             onClick={() => {
+              setLoading(true)
+              fetchSearchedMovies(
+                searchMovieValue.movieTitle,
+                searchMovieValue.releaseDate
+              );
+            }}
+             />
 
-      {movieList.map(({ title, id }) => (
-        <div key={id}>
-          <Link href={`/${id}`}>
-            <p
-              style={{
-                color: "red",
-                backgroundColor: "black",
-                padding: ".5rem",
-                margin: ".3rem 0",
-              }}
-            >
-              {title}
-            </p>
-          </Link>
-        </div>
-      ))}
+      <h1 className="text-5xl my-8 comic-book-title">TRENDNING MOVIES</h1>
+      {movieList.length === 0 && <><h1>Your search - did not match any movies</h1></>}
+<section className="container gap-7 grid justify-center max-w grid-cols-2 md:grid-cols-4 lg:grid-cols-6">
+  
+        {movieList.map(({ title, id, poster_path }) => (
+  <Link key={id} href={`/${id}`} className="flex bg-white border-4 border-black">
+            <div className="max-w-sm overflow-hidden flex flex-col" style={{boxShadow:"8px 7px 0px 3px #1b1d21"}}>
+            <img className="w-full" src={`https://image.tmdb.org/t/p/w500${poster_path}`} alt="Sunset in the mountains"/>
+            <div className="px-3 py-4 flex-grow">
+              <div className="truncate text-lg font-bold mb-2 text-stone-950">{title}</div>
+            </div>
+                  </div>
+                  </Link>
+        ))}
+</section>
     </main>
   );
 }
