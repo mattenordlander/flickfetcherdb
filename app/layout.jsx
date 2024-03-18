f2362a033a034a185f42c2b5ad4fda48eb946100
Mathias Nordlander <mattsson.nord@gmail.com>
2024-03-18 15:36:10 +0100

    Added a header for navigation

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,4 +1,5 @@
 import { Inter } from "next/font/google";
+import Link from "next/link";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -10,7 +11,21 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <header
+          style={{
+            width: "100%",
+            backgroundColor: "hotpink",
+            padding: "0.2em 1rem",
+            marginBottom: "1rem",
+          }}
+        >
+          <Link style={{ textDecoration: "none", color: "white" }} href={"/"}>
+            <h1>LOGO</h1>
+          </Link>
+        </header>
+        {children}
+      </body>
     </html>
   );
 }
