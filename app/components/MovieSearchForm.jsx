a7e2118b19fe28651aecd9fdbee26d13afc3ff11
Mathias Nordlander <mattsson.nord@gmail.com>
2024-03-22 15:55:47 +0100

    New attribute for placeholding title

--- app/components/MovieSearchForm.jsx
+++ app/components/MovieSearchForm.jsx
@@ -6,22 +6,24 @@ export default function SelectElement({
   yearsArray,
   disabled,
   titleValue,
-  onClick
+  onClick,
+  titlePlaceholder
 }) {
   return (
     <form className="flex gap-4 justify-center items-end justify-center p-2">
       <div className="flex flex-col w-3/4 max-w-md">
-        <label className="text-2xl" htmlFor="Search">{inputLabel}</label>
+        <label className="text-xl" htmlFor="Search">{inputLabel}</label>
         <input
           type="text"
           onChange={handleChange}
           value={titleValue}
+          placeholder={titlePlaceholder}
           name={"movieTitle"}
           className=" bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500"
         />
       </div>
       <div className="flex flex-col w-1/4 max-w-md">
-        <label className="text-2xl" htmlFor="Search">{yearLabel}</label>
+        <label className="text-xl" htmlFor="Search">{yearLabel}</label>
         <select
           name="releaseDate"
           onChange={handleChange}
