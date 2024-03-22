0454183686bdc49f1e9a2197c619208935eba892
Mathias Nordlander <mattsson.nord@gmail.com>
2024-03-22 21:35:52 +0100

    Minor design changes

--- app/components/Paigination.jsx
+++ app/components/Paigination.jsx
@@ -12,11 +12,11 @@ const loopPages = () => {
 const moviePages = loopPages();
    
     return (        
-    <nav aria-label="Page navigation">
-      <ul className="inline-flex -space-x-px text-sm">
+    <nav className="flex justify-center" aria-label="Page navigation">
+      <ul className="inline-flex -space-x-px text-xl">
       {moviePages.map((page)=>(
         <li key={page} onClick={onClick}>
-        <a href="#" className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">{page}</a>
+        <a href="#" className="flex px-4 items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">{page}</a>
       </li>
       ))}
         
