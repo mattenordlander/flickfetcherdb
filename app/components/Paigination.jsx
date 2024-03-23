4e155c33add9814f9ff7b38afffee92e28a61355
Mathias Nordlander <mattsson.nord@gmail.com>
2024-03-23 13:45:41 +0100

    OnClick function

--- app/components/Paigination.jsx
+++ app/components/Paigination.jsx
@@ -1,22 +1,24 @@
 "use client"
-export default function Paigination({searchedMoviePage, onClick}){
+export default function Paigination({searchedMoviePage, fetchSearchedMovies, movieTitle,releaseDate}){
 
 
 const loopPages = () => {
     let pagesArray = []
     for (let i = 0; i < searchedMoviePage; i++){
-        pagesArray.push(i)
+        pagesArray.push(i + 1)
     } 
     return pagesArray
 }
 const moviePages = loopPages();
+
+
    
     return (        
     <nav className="flex justify-center" aria-label="Page navigation">
       <ul className="inline-flex -space-x-px text-xl">
       {moviePages.map((page)=>(
-        <li key={page} onClick={onClick}>
-        <a href="#" className="flex px-4 items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">{page}</a>
+        <li key={page} onClick={()=> {fetchSearchedMovies(movieTitle,releaseDate,page)}} className="flex px-4 items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
+        {page}
       </li>
       ))}
         
