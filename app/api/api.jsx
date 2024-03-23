f9c6d280ed74d53a6c9229f8d779e0e9ab2e7523
Mathias Nordlander <mattsson.nord@gmail.com>
2024-03-23 14:40:22 +0100

    Remove conosle.log

--- app/api/api.jsx
+++ app/api/api.jsx
@@ -13,7 +13,6 @@ export async function getMovieList(page) {
         options
       );
       const data = await response.json()
-      console.log(data.results)
       return data.results
     } catch (error) {
       console.error("Error getting movie list: ", error);
@@ -27,7 +26,6 @@ export async function getMovieList(page) {
       }
         const respone = await fetch(`https://api.themoviedb.org/3/search/movie?query=${title}&include_adult=false&language=en-US&primary_release_year=${releaseYear}&page=${page}`, options)
         const searchData = await respone.json();
-        console.log(searchData)
         return searchData
         
     }catch(error){
@@ -39,7 +37,6 @@ export async function getMovieList(page) {
     try{
       const response = await fetch(`https://api.themoviedb.org/3/movie/${movie_id}?language=en-US`, options);
       const movieDetails = await response.json();
-       console.log(movieDetails)
        return movieDetails;
   
     }catch(error){
@@ -51,7 +48,6 @@ export async function getMovieList(page) {
     try{
       const response = await fetch(`https://api.themoviedb.org/3/movie/${movie_id}/credits?language=en-US`, options)
       const creditsList = await response.json();
-      console.log(creditsList);
       return creditsList.cast;
     } catch(error){
       console.error('Get credits list error : ',error);
